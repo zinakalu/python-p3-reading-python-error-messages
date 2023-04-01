2be0aec98e2268e24d35c55f2e6c0e1c2de320ce
zinakalu <zina.kalu@gmail.com>
2023-04-01 08:02:29 -0400

    python p3

--- lib/a_syntax_error.py
+++ lib/a_syntax_error.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 
-poor_syntax = 2 * #
+poor_syntax = 2 * 2
+print(poor_syntax)
--- lib/a_type_error.py
+++ lib/a_type_error.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 
-wrong_type = 'abc' + 123
+wrong_type = 232 + 123
+print(wrong_type)
